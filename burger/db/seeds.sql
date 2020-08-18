7599720b5de577f1ce00bab3c2fbe0651cfc3671
Dilan Li <dilanli0706@gmail.com>
2020-08-18 16:16:10 -0600

    established directory structure and gitignore

---
+++ burger/db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ("Bacon Avocado Burger");
+INSERT INTO burgers (burger_name) VALUES ("Chicken Burger");
+INSERT INTO burgers (burger_name) VALUES ("Vegie Burger");
